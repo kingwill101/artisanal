6ba24e8724da791f6a7dd3faa4d49eac13d432b5
Developer <temp@example.com>
2026-03-19 12:21:14 -0500

    Prefer umbrella imports in control component tests

--- pkgs/artisanal_widgets/test/components/checkbox_test.dart
+++ pkgs/artisanal_widgets/test/components/checkbox_test.dart
@@ -1,5 +1,5 @@
-import 'package:artisanal_widgets/artisanal_widgets.dart';
-import 'package:artisanal_widgets/testing.dart';
+import 'package:artisanal/testing.dart';
+import 'package:artisanal/widgets.dart';
 import 'package:test/test.dart';
 
 void main() {
